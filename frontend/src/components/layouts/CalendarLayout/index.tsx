453784d2dac1909ce23154da18635cb22e59aeaa
Liz <eakorobova2005@mail.ru>
2024-09-28 23:48:12 +0300

    Stylize CalendarLayout

--- frontend/src/components/layouts/CalendarLayout/index.tsx
+++ frontend/src/components/layouts/CalendarLayout/index.tsx
@@ -1,3 +1,6 @@
+import { NavBar } from '@/components/modules/bars';
+import { TopHeader } from '@/components/modules/headers';
+import Head from 'next/head';
 import * as React from 'react';
 
 import { Calendar } from '@/components/ui/calendar';
@@ -6,11 +9,24 @@ export function CalendarLayout() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
-    <Calendar
-      mode='single'
-      selected={date}
-      onSelect={setDate}
-      className='rounded-md border'
-    />
+    <>
+      <Head>
+        <title>Календарь</title>
+      </Head>
+      <div className='grid min-h-screen w-full md:grid-cols-[220px_1fr]'>
+        <NavBar />
+        <div className='flex flex-col overflow-hidden'>
+          <TopHeader />
+          <main className='flex flex-1 flex-col gap-4 overflow-x-auto bg-muted p-4 lg:gap-6 lg:p-6'>
+            <Calendar
+              mode='single'
+              selected={date}
+              onSelect={setDate}
+              className='rounded-md border'
+            />
+          </main>
+        </div>
+      </div>
+    </>
   );
 }
