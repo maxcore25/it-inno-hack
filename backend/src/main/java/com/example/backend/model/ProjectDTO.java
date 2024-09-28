3e74a4c2869d422748ab2ccc23c1ebecfb3b779f
Max <mkorobov2002@mail.ru>
2024-09-28 14:47:08 +0300

    Create ProjectDTO.java

---
+++ backend/src/main/java/com/example/backend/model/ProjectDTO.java
@@ -0,0 +1,42 @@
+package com.example.backend.model;
+
+public class ProjectDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean isDeleted;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+}
