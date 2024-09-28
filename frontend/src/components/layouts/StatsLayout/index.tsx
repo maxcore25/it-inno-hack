ffbde3762b966ac3085071f14f55e1c66504d6b2
Liz <eakorobova2005@mail.ru>
2024-09-28 15:21:15 +0300

    Import AreaChartDemo

--- frontend/src/components/layouts/StatsLayout/index.tsx
+++ frontend/src/components/layouts/StatsLayout/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { LogoHeader } from '@/components/modules/headers/LogoHeader';
+import { AreaChartDemo, PieChartDemo } from '@/components/modules/charts';
 
 export const StatsLayout = () => {
   return (
@@ -8,9 +9,15 @@ export const StatsLayout = () => {
         <title>Статистика</title>
       </Head>
       <LogoHeader />
-      <main className='flex min-h-screen flex-col bg-background'>
-        <div className='theme-zinc flex h-screen w-full items-center justify-center px-4'>
-          <h1>Statistics</h1>
+      <main className='mt-[100px] flex min-h-screen flex-col bg-background'>
+        <div className='theme-zinc flex w-full items-center justify-center px-4'>
+          <h1 className='mb-[100px] text-lg font-semibold md:text-2xl'>
+            Statistics
+          </h1>
+        </div>
+        <div className='grid grid-cols-2 gap-4'>
+          <PieChartDemo />
+          <AreaChartDemo />
         </div>
       </main>
     </>
