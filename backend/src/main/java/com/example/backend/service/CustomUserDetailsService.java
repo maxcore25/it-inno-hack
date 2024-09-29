789401d5d6688b87632025e97c86d921946a3ca4
engory03 <mkorobov2002@mail.ru>
2024-09-29 09:40:08 +0300

    Add jwt

---
+++ backend/src/main/java/com/example/backend/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.backend.service;
+
+import com.example.backend.repository.UserRepository;
+import com.example.backend.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
